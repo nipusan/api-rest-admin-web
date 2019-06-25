f0143b622e2f484fb2a82df34275c29e25c39980
Nixon Sanchez <nixon.sanchez@ingenian.com>
2019-06-25 09:21:48 -0500

    Init api rest JSON in Spring Boot java

---
+++ src/main/java/com/loya/springjpaoracledemo/SpringJpaOracleDemoApplication.java
@@ -0,0 +1,14 @@
+package com.loya.springjpaoracledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SpringJpaOracleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaOracleDemoApplication.class, args);
+    }
+}
